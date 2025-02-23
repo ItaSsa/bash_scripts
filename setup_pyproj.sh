fb767353d20440be35deb9a0045a8b36682657ff
ItaSsa <itairassa@gmail.com>
2025-02-23 16:23:27 +0100

    script to scaffolding python projects

---
+++ setup_pyproj.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#she bang to take the patern bash from your variable $PATH.
+
+# This file create a project folder with the informed name and create subfolders
+
+# If the project name is missing
+if [ "$#" -ne 1 ]; then
+    echo "Inform  the path for the new project: like this $0 project_path"
+    exit 1
+fi
+
+projdir="$PWD/$1"
+#echo $projedir
+
+# Check if the directory exists
+if [ -d "$projdir" ]; then
+   echo "The directory '$projdir' already exists"
+   read -p "Do you want recreate the folder?(Y/N)" option
+   if [ "$option" == "N" ]; then
+      echo "Nothing to do.Exiting!"
+      exit  1
+   else
+      # Deleting directory
+      rm -r $projdir	  
+   fi
+
+fi
+
+# Creating the project scaffolding:
+# 1. Project folder and subfoders
+mkdir -p "$projdir" "$projdir/app" "$projdir/docs" "$projdir/tests"
+# 2. The main.py
+touch "$projdir/main.py"
+# 3. The readme.md
+touch "$projdir/README.md"
+echo "Scaffolding for $0 is ok! :) "
+
+echo "Opening in the VSCODE" 
+#Moving to the project folder
+code $projedir
